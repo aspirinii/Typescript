1d46994da3042c0470fa5677228efac1657b81d7
aspirinii <aspiriniijk@gmail.com>
2021-12-10 14:05:32 +0900

    chapter8

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,24 @@
-class Block{
-    public index:number;
-    public hash:string;
+import * as CryptoJS from "crypto-js";
+
+class Block {
+    public index: number;
+    public hash: string;
     public previousHash: string;
     public data: string;
     public timestamp: number;
+    // static method : I can use method wihout making Block
+
+    sayHello = ():string => "hellog"
+    static calculateBlockHash = (index: number, previousHash: string, timestamp: number, data: string): string => CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+
     constructor(
-        index:number,
-        hash:string,
+        index: number,
+        hash: string,
         previousHash: string,
         data: string,
         timestamp: number
-    ){
-        this.index =index;
+    ) {
+        this.index = index;
         this.hash = hash;
         this.previousHash = previousHash;
         this.data = data;
@@ -19,12 +26,22 @@ class Block{
     }
 }
 
-const genesisBlock :Block = new Block(0, "2020202020", "", "Hello", 123456);
+
+const genesisBlock: Block = new Block(0, "2020202020", "", "Hello", 123456);
 
 let blockchain: [Block] = [genesisBlock];
 console.log(blockchain);
 
-console.log("second");
+
+const getBlockchain = () : Block[] => blockchain;
+const getlatestBlcok = () : Block => blockchain[blockchain.length -1];
+
+const getNewTimesStamp = (): number => Math.round(new Date().getTime()/1000);
+
+console.log(genesisBlock.sayHello());
+// console.log(sayHello());
+console.log("secon2");
 
 
-export{};
+
+export {};
